59dd55645c9cedbd203d571e386e76206974a28a
AybarsSeyrek <161784475+AybarsSeyrek@users.noreply.github.com>
2026-05-16 23:13:54 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,27 @@
+# Python calculator
 
+operator = input("Enter an operator (+ - * /): ") #asks the user for arithmetic operator
+
+try: # try / except is used to handle errors so the program does not crash.
+    num1 = float(input("Enter the 1st number: ")) # to avoid string concatenation use Type casting (turn string to float)
+    num2 = float(input("Enter the 2nd number: "))
+
+    if operator == "+":
+        result = num1 + num2
+        print(round(result, 3))
+    elif operator == "-":
+        result = num1 - num2
+        print(round(result, 3))
+    elif operator == "*":
+        result = num1 * num2
+        print(round(result, 3))
+    elif operator == "/":
+        if num2 == 0:
+            print("You cannot divide by zero")
+        else:
+            result = num1 / num2
+            print(round(result, 3))
+    else:
+        print(f"{operator} is not valid operator")
+except ValueError: 
+    print("Invalid number. Please enter a real number.")
